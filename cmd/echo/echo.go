a9e977c57b7b84559fa9b975bc5c3d12e72c5812
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-02-05 22:01:15 +0200

    feat: echo workload

--- cmd/echo/echo.go
+++ cmd/echo/echo.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+    "encoding/json"
+    "log"
+
+    maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+// Follow the lead from the library, simpler than dealing with
+// all kinds of maps and reclection and whatnot
+type EchoBody struct {
+	maelstrom.MessageBody
+	Echo string `json:"echo,omitempty"`
+}
 
 func main() {
-
+	node := maelstrom.NewNode()
+	node.Handle("echo", func(msg maelstrom.Message) error {
+		var body EchoBody
+		if err := json.Unmarshal(msg.Body,&body); err != nil {
+			return err
+		}
+		body.Type = "echo_ok";
+		return node.Reply(msg, body)
+	})
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
