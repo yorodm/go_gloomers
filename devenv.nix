0a077dd2df549763e980e7a4ad18bdd8921825ac
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-02-05 21:44:10 +0200

    initial: let there be light

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  languages.go.enable = true;
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
