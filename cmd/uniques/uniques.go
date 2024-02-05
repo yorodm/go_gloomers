f2ec3939ba0f1dbe4de594ff1351144fac839ae6
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-02-05 23:03:43 +0200

    fix: I'm not using crypto/rand for this

--- cmd/uniques/uniques.go
+++ cmd/uniques/uniques.go
@@ -3,28 +3,26 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"math/rand"
-	"time"
-
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 // I'm not making two structs for this
 type UniqueBody struct {
 	maelstrom.MessageBody
-	Id uint32 `json:"id,omitempty"`
+	Id string `json:"id,omitempty"`
 }
 
+
 func main() {
-	r := rand.New(rand.NewSource(time.Now().UnixMilli())) // this is enough
 	node := maelstrom.NewNode()
 	node.Handle("generate", func(msg maelstrom.Message) error {
 		var body UniqueBody;
 		if err := json.Unmarshal(msg.Body,&body); err != nil {
 			return err
 		}
+		body.Id = uuid.New().String()
 		body.Type = "generate_ok";
-		body.Id = r.Uint32()
 		return node.Reply(msg,body)
 	});
 	if err := node.Run(); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yorodm/go_gloomers
 
 go 1.21
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231231190402-2674df7c1076
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231231190402-2674df7c1076
+)
